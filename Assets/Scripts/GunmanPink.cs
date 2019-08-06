b725ede0e602bd04c9601f31c5868ca7c2055672
Adriano Sudário <sudario.adriano@gmail.com>
2019-08-05 21:53:49 -0300

    Initial Commit

---
+++ Assets/Scripts/GunmanPink.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunmanPink : MonoBehaviour
+{
+    BulletController bullet;
+    GameController gameController;
+    Vector3 middleOfScreen;
+    bool canShoot = true;
+    List<BulletController> bullets = new List<BulletController>();
+    
+    void Start()
+    {
+        bullet = GetComponentInChildren<BulletController>();
+        bullet.Hide();
+        middleOfScreen = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
+        gameController = FindObjectOfType<GameController>();
+    }
+    
+    void Update()
+    {
+        Vector3 cameraVector = Input.mousePosition - middleOfScreen;
+        Vector3 flipped = new Vector3(cameraVector.x, 0f, cameraVector.y);
+        transform.LookAt(flipped);
+
+        if (Input.GetMouseButtonDown(0) && canShoot)
+            StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        BulletController bulletInstance = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
+        bulletInstance.Appear();
+        bullets.Add(bulletInstance);
+        yield return new WaitForSeconds(1);
+        canShoot = true;
+    }
+}
---
+++ Assets/Scripts/MosquitoController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MosquitoController : MonoBehaviour
+{
+    Rigidbody body;
+    Vector3 movement;
+    bool isDead = false;
+
+    public float speed = 1f;
+    public float deathRotationSpeed = 100f;
+
+    void Start()
+    {
+        movement = new Vector3(speed, 0, 0);
+        body = GetComponent<Rigidbody>();
+        //transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+
+        // spawns: 
+        // x: entre -6 e 6
+        // z: entre 1 e -1.5
+    }
+
+    void FixedUpdate()
+    {
+        if (isDead)
+            Spin();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Bullet")
+            return;
+        
+        body.useGravity = true;
+        body.AddForce(new Vector3(0, 2, 0), ForceMode.Impulse);
+        isDead = true;
+        Destroy(collider.gameObject);
+    }
+
+    void Move()
+    {
+        body.AddForce(movement, ForceMode.Force);
+    }
+
+    void Spin()
+    {
+        transform.Rotate(new Vector3(0, 0, deathRotationSpeed) * Time.deltaTime, Space.Self);
+    }
+}
